8bd218992c4c1a9c4a03f0558ee661562cf7dc8c
Magdalena Trzcińska <trzcinskamagdalena01@gmail.com>
2023-05-17 16:50:54 +0200

    Add address model

---
+++ CompanyStruture/Models/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyStruture.Models
+{
+    public class Address
+    {
+        public int? Id { get; set; } = null;
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+        public int? ApartmentNumber { get; set; } = null;
+        public string[] getAddress()
+        {
+            string[] data = new string[4];
+            data[0] = City;
+            data[1] = Street;
+            data[2] = BuildingNumber;
+            data[3] = ApartmentNumber == null ? "" : "/" + ApartmentNumber.ToString();
+            return data;
+        }
+    }
+}
--- CompanyStruture/Models/Employee.cs
+++ CompanyStruture/Models/Employee.cs
@@ -9,14 +9,11 @@ namespace CompanyStruture.Models
 {
     public class Employee
     {
-        public int Id { get; set; }
+        public int? Id { get; set; } = null;
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Pesel { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-        public string BuildingNumber { get; set; }
-        public int? ApartmentNumber { get; set; } = null;
+        public Address Address { get; set; }
 
         public string[] getPersonalData()
         {
@@ -25,16 +22,6 @@ namespace CompanyStruture.Models
             data[1] = Name;
             data[2] = Surname;
             data[3] = Pesel;
-            return data;
-        }
-
-        public string[] getAddress()
-        {
-            string[] data = new string[4];
-            data[0] = City;
-            data[1] = Street;
-            data[2] = BuildingNumber;
-            data[3] = ApartmentNumber == null ? "" : "/"+ApartmentNumber.ToString();
             return data;
         }
     }
